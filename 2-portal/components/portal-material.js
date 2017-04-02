130dce4f543335d48257c1a84d8ab0d21a1ec6c2
Don McCurdy <don.r.mccurdy@gmail.com>
2017-04-01 23:07:23 -0700

    Reorganize.

--- 2-portal/components/portal-material.js
+++ 2-portal/components/portal-material.js
@@ -1,102 +1,6 @@
-const AFRAME = require('aframe'),
-      glsl = require('glslify'),
-      vertexShader = glsl.file('../shaders/portal-material.vertex.glsl'),
-      fragmentShader = glsl.file('../shaders/portal-material.fragment.glsl');
+const AFRAME = require('aframe');
 
 const textureLoader = new THREE.TextureLoader();
-
-AFRAME.registerComponent('portal', {});
-
-module.exports.System = AFRAME.registerSystem('portal', {
-  schema: {
-    camera: {type: 'selector', default: '[camera]'},
-    portal: {type: 'selector', default: '[portal]'}
-  },
-
-  init: function () {
-    this.materials = new Set();
-
-    this.cameraSide = 1.0; // 1.0 | -1.0
-    this.portalPosition = new THREE.Vector3();
-    this.portalNormal = new THREE.Vector3(0, 0, 1);
-    this.portalRadius = 0.8;
-    this.prevPosition = null;
-
-    this.updateTracking();
-  },
-
-  /**
-   * Update all uniforms passed as inputs to the shader.
-   */
-  tick: function () {
-    this.updateTracking();
-
-    const materials = Array.from(this.materials);
-    for (var i = 0, material; (material = materials[i]); i++) {
-      material.uniforms.cameraSide.value = this.cameraSide;
-      material.uniforms.vPortalPosition.value.copy(this.portalPosition);
-      material.uniforms.vPortalNormal.value.copy(this.portalNormal);
-    }
-  },
-
-  createPortalMaterial: function (inputUniforms) {
-    const uniforms = AFRAME.utils.extend({
-      cameraSide:      {value: this.cameraSide},
-      vPortalPosition: {value: this.portalPosition},
-      vPortalNormal:   {value: this.portalNormal},
-      portalRadius:    {value: this.portalRadius}
-    }, inputUniforms);
-
-    const material = new THREE.ShaderMaterial({
-      uniforms: uniforms,
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader
-    });
-
-    this.materials.add(material);
-
-    return material;
-  },
-
-  removePortalMaterial: function (material) {
-    this.materials.delete(material);
-  },
-
-  /**
-   * Detects when the camera passes through the portal and inverts the shader.
-   */
-  updateTracking: (function () {
-    const portalPlane = new THREE.Plane(),
-          portalIntersection = new THREE.Vector3(),
-          currentPosition = new THREE.Vector3(),
-          displacementSegment = new THREE.Line3(new THREE.Vector3(), new THREE.Vector3());
-
-    return function () {
-      const data = this.data,
-            prevPosition = this.prevPosition;
-
-      currentPosition.copy(data.camera.getAttribute('position'));
-      this.portalPosition.copy(data.portal.getAttribute('position'));
-
-      if (prevPosition && prevPosition.equals(currentPosition)) {
-        return;
-      } else if (prevPosition) {
-        displacementSegment.start.copy(prevPosition);
-        displacementSegment.end.copy(currentPosition);
-        portalPlane.setFromNormalAndCoplanarPoint(this.portalNormal, this.portalPosition);
-
-        if (portalPlane.intersectLine(displacementSegment, portalIntersection)
-            && portalIntersection.distanceTo(this.portalPosition) < this.portalRadius) {
-          this.cameraSide *= -1;
-        }
-      } else {
-        this.prevPosition = new THREE.Vector3();
-      }
-
-      this.prevPosition.copy(currentPosition);
-    };
-  }())
-});
 
 module.exports.Component = AFRAME.registerComponent('portal-material', {
   dependencies: ['material', 'geometry'],
@@ -104,7 +8,7 @@ module.exports.Component = AFRAME.registerComponent('portal-material', {
   schema: {
     side: {default: 'A', oneOf: ['A', 'B']},
     color: {type: 'color'},
-    map: {type: 'src'}
+    map: {type: 'asset'}
   },
 
   init: function () {
--- 2-portal/index.js
+++ 2-portal/index.js
@@ -1,5 +1,6 @@
-const AFRAME = require('aframe');
+require('aframe');
 
 // Components.
 require('aframe-teleport-controls');
+require('./components/portal');
 require('./components/portal-material');
